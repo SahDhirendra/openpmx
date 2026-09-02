dd2027b59b8eb6cb15c97f1723c98663abb7eca2
Dhirendra Sah <sah.dhirendra0101@gmail.com>
2026-09-01 22:09:25 -0500

    Fix CORS - add 192.168.1.3 to allowed origins for Pi kiosk display

--- app/main.py
+++ app/main.py
@@ -45,6 +45,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
+        "http://192.168.1.3:5173",
         "https://openpmx-frontend.onrender.com"
     ],
     allow_credentials=True,
